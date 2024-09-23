87e2aa442ba4a53d354419c218d0ecba7422d996
Saulobf321 <saulobf8@gmail.com>
2024-09-23 20:09:32 -0300

    aaaaaaaa

--- Pessoa_SytemDP1/src/tech/saulo/composicao/Enderco.java
+++ Pessoa_SytemDP1/src/tech/saulo/composicao/Enderco.java
@@ -1,8 +1,58 @@
 package tech.saulo.composicao;
 
 public class Enderco {
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
     private String rua;
     private String numero;
     private String pais;
     private String cep;
+    private String estado;
+    private String cidade;
 }
